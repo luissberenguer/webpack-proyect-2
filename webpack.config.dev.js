e2a25d20559f40d30b17e787c77b47285bffce9b
Luis Berenguer <luis01developer@gmail.com>
2022-03-04 17:36:21 +0100

    fix buG

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -12,7 +12,7 @@ module.exports = {
   mode: "development",
   resolve: {
     extensions: [".js", ".jsx"],
-    alis: {
+    alias: {
       "@components": path.resolve(__dirname, "src/components/"),
       "@style": path.resolve(__dirname, "src/styles/"),
     },
